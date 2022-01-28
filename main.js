c92bd7ffe1b675289b658443886a0f4e557afb0c
Rashad Nelson <rashadnelson@gmail.com>
2022-01-28 13:34:44 -0500

    CodeWars - 01/28/2022

--- main.js
+++ main.js
@@ -126,6 +126,7 @@ function findEvenIndex(arr) {
 findEvenIndex([15, 10, 5, 10, 10]);
 */
 
+/*
 function toCamelCase(str) {
 	if (str.includes('-')) {
 		let newStr = str.replace('-', '');
@@ -136,3 +137,36 @@ function toCamelCase(str) {
 }
 
 toCamelCase('the-stealth-warrior');
+*/
+
+// Create a function taking a positive integer as its parameter and returning a string containing the Roman Numeral representation of that integer.
+
+// Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
+
+const intToRoman = (num) => {
+	const map = {
+		M: 1000,
+		CM: 900,
+		D: 500,
+		CD: 400,
+		C: 100,
+		XC: 90,
+		L: 50,
+		XL: 40,
+		X: 10,
+		IX: 9,
+		V: 5,
+		IV: 4,
+		I: 1,
+	};
+
+	let result = '';
+
+	// main algorithm
+	for (key in map) {
+		result += key.repeat(Math.floor(num / map[key]));
+		num %= map[key];
+	}
+
+	return result;
+};
